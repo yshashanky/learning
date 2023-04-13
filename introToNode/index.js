10d3133a66303aaeaaa7a046957b219041d377a1
yshashanky <812shanky@gmail.com>
2023-04-13 23:23:47 +0530

    nodeStarter

---
+++ introToNode/index.js
@@ -0,0 +1,11 @@
+// const fs = require("fs");
+// fs.copyFileSync("sample.txt", "copied.txt");
+
+// var superheroes = require("superheroes");
+// var myName = superheroes.random();
+// console.log(myName);
+
+
+var supervillains = require("supervillains");
+var myName = supervillains.random();
+console.log(myName);
