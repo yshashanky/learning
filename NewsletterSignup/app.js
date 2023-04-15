355497a83dfaa92fc14642cc98154981d648c345
yshashanky <812shanky@gmail.com>
2023-04-15 18:20:31 +0530

    newsLetter

--- NewsletterSignup/app.js
+++ NewsletterSignup/app.js
@@ -2,8 +2,10 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const axios = require("axios");
 const https = require("node:https");
+const exp = require("node:constants");
 
 const app = express();
+app.use(express.static("Public"));
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", function(req, res){
