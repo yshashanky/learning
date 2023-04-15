0913365f37ac90b0e2b6f665800a6136a83a47de
yshashanky <812shanky@gmail.com>
2023-04-15 18:10:42 +0530

    newsletter

---
+++ NewsltterSignup/app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const axios = require("axios");
+const https = require("node:https");
+
+const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", function(req, res){
+    res.send("working");
+});
+
+app.listen(3000, function(){
+    console.log("server is running on port 3000...");
+});
