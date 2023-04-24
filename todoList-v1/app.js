3afb34266b9480a32c7a23f0551becea300d8e0b
yshashanky <812shanky@gmail.com>
2023-04-24 23:24:47 +0530

    toDoUpdate

--- todoList-v1/app.js
+++ todoList-v1/app.js
@@ -3,12 +3,13 @@ const bodyParser = require("body-parser");
 
 const app = express();
 app.set("view engine", "ejs");
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("./public"));
 
 let items = ["Eat", "Sleep", "Repeat"];
+let workItems = [];
 
-app.get("/", function(req, res){
+app.get("/", function (req, res) {
 
     let todayDay = new Date();
     let options = {
@@ -18,15 +19,24 @@ app.get("/", function(req, res){
     }
     let day = todayDay.toLocaleDateString('en-US', options);
 
-    res.render('list', {todaysDay: day, newListItems: items});
+    res.render('list', { listTitle: day, newListItems: items });
 });
 
-app.post("/", function(req, res){
+app.get("/work", function (req, res) {
+    res.render("list", { listTitle: "Work List", newListItems: workItems })
+});
+
+app.post("/", function (req, res) {
     let item = req.body.item;
-    items.push(item);
-    res.redirect("/");
+    if (req.body.list === "Work List") {
+        workItems.push(item);
+        res.redirect("/work");
+    } else {
+        items.push(item);
+        res.redirect("/");
+    }
 });
 
-app.listen(3000, function(req, res){
+app.listen(3000, function (req, res) {
     console.log("Server started on 3000");
 });
