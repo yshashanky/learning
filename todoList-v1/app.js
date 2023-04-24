c10cf5b25d2f0debdc12c6e3ab5124bee9a6cf2b
yshashanky <812shanky@gmail.com>
2023-04-24 22:05:28 +0530

    todoListUpdate

--- todoList-v1/app.js
+++ todoList-v1/app.js
@@ -3,14 +3,27 @@ const bodyParser = require("body-parser");
 
 const app = express();
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
 
-app.get("/", function(req, res){
+app.get("/", function (req, res) {
 
     var today = new Date();
+    var options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long"
+    };
 
-    res.send("Hello")
+    var day = today.toLocaleDateString("en-US", options);
+    res.render('list', { kindOfDay: day });
+
 });
 
-app.listen(3000, function(req, res){
+app.post("/", function(req, res){
+    var item = req.body.newItem;
+    console.log(item);
+});
+
+app.listen(3000, function (req, res) {
     console.log("Server started on port 3000")
 });
