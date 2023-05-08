a1766aff15c16e4a7e1e29f76a02103f1aa0686a
yshashanky <812shanky@gmail.com>
2023-05-08 21:59:05 +0530

    update

--- toDoList/app.js
+++ toDoList/app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const date = require(__dirname + "/date.js");
+const mongoose = require("mongoose");
 
 const app = express();
 app.set("view engine", "ejs");
@@ -10,8 +10,32 @@ app.use(express.static("./public"));
 const items = ["Eat", "Sleep", "Repeat"];
 const workItems = [];
 
+mongoose.connect("mongodb://127.0.0.1:27017/todolistDB", { useNewUrlParser: true });
+
+const itemsSchema = {
+    name: String,
+};
+
+const Item = mongoose.model("Item", itemsSchema);
+
+const defaultItem1 = new Item({
+    name: "Wake up..."
+});
+
+const defaultItem2 = new Item({
+    name: "Get back to your senses"
+});
+
+const defaultItem3 = new Item({
+    name: "Don't burst on anyone"
+});
+
+const defaultItem = [defaultItem1, defaultItem2, defaultItem3];
+
+Item.insertMany(defaultItem);
+
 app.get("/", function (req, res) {
-    res.render('list', { listTitle: date.getDay(), newListItems: items });
+    res.render('list', { listTitle: "Today", newListItems: items });
 });
 
 app.get("/work", function (req, res) {
