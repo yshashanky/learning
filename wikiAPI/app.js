805059822be324c99b127342945197e02b95b15b
yshashanky <812shanky@gmail.com>
2023-05-10 21:38:14 +0530

    wikiDBStart

---
+++ wikiAPI/app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+const app = express();
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("./public"));
+
+mongoose.connect("mongodb://127.0.0.1:27017/wikiDB", { useNewUrlParser: true });
+
+const articleSchema = {
+    title: String,
+    content: String
+};
+
+const Article = mongoose.model("Article", articleSchema);
+
+
+app.listen(3000, function (req, res) {
+    console.log("Server started on port 3000");
+});
