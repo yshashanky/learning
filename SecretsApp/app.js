8bf753b0c6fd5eda6f2a7c90807cf9336dc09670
yshashanky <812shanky@gmail.com>
2023-05-12 00:53:40 +0530

    update

--- SecretsApp/app.js
+++ SecretsApp/app.js
@@ -10,6 +10,13 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 mongoose.connect("mongodb://127.0.0.1:27017/usersDB", { useNewUrlParser: true });
 
+const userSchema = {
+    email: String,
+    password: String,
+};
+
+const User = new mongoose.model("User", userSchema);
+
 app.get("/", function(req, res){
     res.render("home");
 });
@@ -20,6 +27,37 @@ app.get("/login", function(req, res){
 
 app.get("/register", function(req, res){
     res.render("register");
+});
+
+app.post("/register", function(req, res){
+    const newUser = new User({
+        email: req.body.username,
+        password: req.body.password
+    });
+
+    newUser.save().then((data) => {
+        console.log(data);
+        res.render("secrets");
+    });
+
+});
+
+app.post("/login", function(req, res){
+    const username = req.body.username;
+    const password = req.body.password;
+
+    User.findOne({email: username}).then((data) => {
+        console.log(data);
+        if (data === null) {
+            res.render("home");
+        } else {
+            if(data.password === password) {
+                res.render("secrets");
+            } else {
+                res.render("home");
+            }
+        }
+    });
 });
 
 app.listen(3000, function (req, res) {
