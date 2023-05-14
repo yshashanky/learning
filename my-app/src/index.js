87489c925eb1fb9bcd69b118cb611224554df938
yshashanky <812shanky@gmail.com>
2023-05-14 11:58:31 +0530

    update

--- my-app/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <h1>Hello</h1>
-  </React.StrictMode>
-);
