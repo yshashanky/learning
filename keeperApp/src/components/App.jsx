6c275464c42539470e9f83030f1783188e75a6c8
yshashanky <812shanky@gmail.com>
2023-05-17 23:09:34 +0530

    update

--- keeperApp/src/components/App.jsx
+++ keeperApp/src/components/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
@@ -6,8 +6,20 @@ import CreateArea from "./CreateArea";
 
 function App() {
 
+  const [notes, setNotes] = useState([]);
+
   function addNote(note) {
-    console.log(note);
+    setNotes(prevNotes => {
+      return [...prevNotes, note]
+    })
+  }
+
+  function deleteNote(id) {
+    setNotes(prevNotes => {
+      return prevNotes.filter((noteItem, index) => {
+        return index !== id;
+      })
+    })
   }
 
   return (
@@ -16,7 +28,9 @@ function App() {
       <CreateArea 
         onAdd = {addNote}
       />
-      <Note key={1} title="Note title" content="Note content" />
+      {notes.map((noteItem, index) => {
+        return <Note key={index} id={index} title={noteItem.title} content={noteItem.content} onDelete={deleteNote}/>
+      })}
       <Footer />
     </div>
   );
